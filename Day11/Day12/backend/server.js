2af46d68dec4976f7836b7f757c37fcf7832084f
SRINITHIk16 <srinithi.k2024aids@sece.ac.in>
2025-12-18 22:32:10 +0530

    Add files via upload

---
+++ Day11/Day12/backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import planRoutes from "./routes/planRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/plans", planRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.listen(process.env.PORT || 5000, () =>
+    console.log(`Server running on port ${process.env.PORT || 5000}`)
+);
